f4a76c0c116cb2457a564a4a42ea448c1345c477
hsedaqat1378 <71232870+hsedaqat1378@users.noreply.github.com>
2021-07-22 15:43:17 +0430

    Update Routes.js

--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -2,7 +2,6 @@ import Home from '../components/Home.vue'
 import About from '../components/About.vue'
 import Contact from '../components/Contact.vue'
 
-
 export const router = [
     { path: '/', component: Home },
     { path: '/About', component: About },
